118d3bdbdcfb2423ecf548eabb7da758da7db303
Jeroen Wouters <jeroen@diasporist.org>
2023-05-19 09:32:58 +0100

    remove old build script

--- src/plasim/cmake_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-INSTALLDIR=$HOME/.local/
-mkdir build
-cd build
-NLAT=64
-NLEV=10
-NPRO=2
-cmake -Dnlat=${NLAT} -Dnlev=${NLEV} -Dnpro=${NPRO} -DCMAKE_INSTALL_PREFIX=${INSTALLDIR} .. && make && make install		
